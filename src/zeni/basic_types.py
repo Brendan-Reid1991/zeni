5059b1b4634e37c366514fc8d030ee0c9cb80780
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2025-11-13 21:45:35 +0000

    updated basic types

--- src/zeni/basic_types.py
+++ src/zeni/basic_types.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from enum import Enum
+from decimal import Decimal
+from enum import Enum, auto
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -15,84 +16,99 @@ class Transaction:
 
     date: date
     description: str
-    amount: float
+    amount: Decimal
     account: str
     currency: str | None = None
     uuid: str | None = None
 
 
-class Account(str, Enum):
+class ZeniStrEnum(str, Enum):
+    """A base class for all str enums in Zeni."""
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.value
+
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower()
+
+
+class Account(ZeniStrEnum):
     """Account enum."""
 
-    DEBIT = "debit"
-    CREDIT = "credit"
-    SAVINGS = "savings"
-    ISA = "isa"
+    DEBIT = auto()
+    CREDIT = auto()
+    SAVINGS = auto()
+    ISA = auto()
 
 
-class Payment(str, Enum): ...
+class Payment(ZeniStrEnum): ...
 
 
 class Outgoing(Payment):
     """Outgoing payment categories."""
 
-    RENT = "rent"
-    BILL = "bill"
-    SUBSCRIPTION = "subscription"
-    CASH_WITHDRAWAL = "cash_withdrawal"
-    TRANSFER_OUT_EXTERNAL = "transfer_out_external"
-    INVESTMENT = "investment"
-    FEE = "fee"
-    LOAN_PAYMENT = "loan_payment"
-    CREDIT_CARD = "credit_card"
-    LEISURE = "leisure"
-    UNCATEGORISED = "uncategorised"
-    GROCERIES = "groceries"
+    RENT = auto()
+    BILL = auto()
+    SUBSCRIPTION = auto()
+    CASH_WITHDRAWAL = auto()
+    TRANSFER_OUT_EXTERNAL = auto()
+    INVESTMENT = auto()
+    FEE = auto()
+    LOAN_PAYMENT = auto()
+    CREDIT_CARD = auto()
+    LEISURE = auto()
+    UNCATEGORISED = auto()
+    GROCERIES = auto()
 
 
 class Incoming(Payment):
     """Incoming payment categories."""
 
-    SALARY = "salary"
-    INTEREST = "interest"
-    REFUND = "refund"
-    TRANSFER_IN_EXTERNAL = "transfer_in_external"
-    CASH_DEPOSIT = "cash_deposit"
-    INCOME = "income"
+    SALARY = auto()
+    INTEREST = auto()
+    REFUND = auto()
+    TRANSFER_IN_EXTERNAL = auto()
+    CASH_DEPOSIT = auto()
+    INCOME = auto()
 
 
 class Internal(Payment):
     """Internal payment categories."""
 
-    TRANSFER = "transfer"
-    SAVINGS = "savings"
-    ROUNDUP = "roundup"
+    TRANSFER = auto()
+    SAVINGS = auto()
+    ROUNDUP = auto()
 
 
-class StandardColumns(str, Enum):
-    DATE = "date"
-    TIME = "time"
-    CATEGORY = "category"
-    NAME = "name"
-    AMOUNT = "amount"
-    CURRENCY = "currency"
-
-
+class StandardColumns(ZeniStrEnum):
+    DATE = auto()
+    NAME = auto()
+    NOTES = auto()
+    CATEGORY = auto()
+    AMOUNT = auto()
+    CURRENCY = auto()
+    BALANCE = auto()
 
 
 class Addressable:
     def __init__(self, iterable: Iterable[str]):
         self.iterable = iterable
+        if not all(isinstance(x, str) for x in iterable):
+            raise ValueError(
+                "To make an iterable addressable all elements must be strings."
+            )
         for item in self.iterable:
-            attr_name = item.replace(" ", "_").replace("-", "_")
+            attr_name = item.lower().replace(" ", "_").replace("-", "_")
             if attr_name.isidentifier():
                 setattr(self, attr_name, item)
             else:
-                raise ValueError(
-                    f"Cannot convert '{item}' to a valid attribute name."
-                )
+                raise ValueError(f"Cannot convert '{item}' to a valid attribute name.")
 
-    def __iter__(self) -> Iterator[str  ]:
+    def __iter__(self) -> Iterator[str]:
         return self.iterable.__iter__()
 
     def __len__(self) -> int:
