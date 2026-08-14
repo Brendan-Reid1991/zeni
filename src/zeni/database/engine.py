e3a3e5106bdabe11fcdf90dd8e878a3300626ab9
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2026-08-14 11:32:06 +0100

    Adding foreign key verification and renaming class to Engineer

--- src/zeni/database/engine.py
+++ src/zeni/database/engine.py
@@ -3,13 +3,15 @@
 from pathlib import Path
 
 from sqlalchemy import Engine as SQLEngine
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine.interfaces import DBAPIConnection
+from sqlalchemy.pool import ConnectionPoolEntry
 
 from zeni.database.models import ZeniBase
 from zeni.database.utils import DEFAULT_PATHWAY, sql_directory
 
 
-class Engine:
+class Engineer:
     """A wrapper around SQLAlchemy Engine.
 
     Parameters
@@ -31,12 +33,21 @@ class Engine:
         self.echoes = echoes
 
         self._db = sql_directory(self.pathway, self.name)
-        self._sql: SQLEngine = create_engine(self._db, echo=self.echoes)
-        ZeniBase.metadata.create_all(self._sql)
+        self.engine: SQLEngine = create_engine(self._db, echo=self.echoes)
+
+        @event.listens_for(self.engine, "connect")
+        def _enable_foreign_keys(
+            dbapi_connection: DBAPIConnection, connection_record: ConnectionPoolEntry
+        ) -> None:
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
+
+        ZeniBase.metadata.create_all(self.engine)
 
     def close_connections(self) -> None:
         """Close all connections."""
-        self._sql.dispose()
+        self.engine.dispose()
 
     def delete(self) -> None:
         """Delete the database file if it exists."""
